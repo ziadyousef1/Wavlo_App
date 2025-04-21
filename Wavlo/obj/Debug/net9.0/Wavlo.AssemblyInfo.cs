4c2108a1fc6ae9e7adfb41bc6563fd46a5bfb32a
Shahd Yassin <shahdyassin63@gmail.com>
2025-04-21 11:29:06 +0200

    Add Resend Otp

--- Wavlo/obj/Debug/net9.0/Wavlo.AssemblyInfo.cs
+++ Wavlo/obj/Debug/net9.0/Wavlo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wavlo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6590ce3986fa55f7cda49138b4895cd5598c205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db27497e766b3460e1e3c1177f6fbc04ec17b886")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wavlo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wavlo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
